adce8f1e67149be3d60d38635a8a85070e9d3d35
Phantom <2909981736@qq.com>
2024-03-06 09:05:20 +0800

    CWRU

--- data_preprocess/CWRU/pack_data.py
+++ data_preprocess/CWRU/pack_data.py
@@ -29,9 +29,9 @@ def pack_data(data_path, load, save_path, length=1024, step_size=300, count=200,
             all_load = glob(class_ + '/*.npy')
             for one_load in all_load:
                 one_load_data = np.load(one_load)
-                window_data = [one_load_data[i:i + length] for i in
-                               range(0, len(one_load_data) - length + 1, step_size)]
-                class_data.append(window_data)
+                for sub in range(len(one_load_data)):
+                    class_data.append([one_load_data[sub][i:i + length] for i in
+                                       range(0, len(one_load_data[sub]) - length + 1, step_size)])
             window_data = np.concatenate(class_data)
             if count > len(window_data):
                 raise ValueError(f"所需数据长度超过数组长度 count({count}) > window_data({len(window_data)})")
@@ -48,8 +48,11 @@ def pack_data(data_path, load, save_path, length=1024, step_size=300, count=200,
         all_sample_data = []
         for class_ in classes:
             class_data = np.load(class_ + "/" + str(load) + '.npy')
-            window_data = np.array(
-                [class_data[i:i + length] for i in range(0, len(class_data) - length + 1, step_size)])
+            all_window_data = []
+            for sub in range(len(class_data)):
+                all_window_data.append(
+                    [class_data[sub][i:i + length] for i in range(0, len(class_data[sub]) - length + 1, step_size)])
+            window_data = np.concatenate(all_window_data)
             if count > len(window_data):
                 raise ValueError(f"所需数据长度超过数组长度 count({count}) > window_data({len(window_data)})")
             sample_index = np.random.choice(np.arange(0, len(window_data)), size=count, replace=False)
